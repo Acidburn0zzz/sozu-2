e793e18190180f7a2340c6d9acb3aa7196a0c459
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-01-28 01:49:37 -0800

    Natter
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- sozu-deploy.sh
+++ sozu-deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-git config --global user.name "Haskell Pushbot"
-git config --global user.email "haskell-pushbot@$(hostname)"
+
+# Configuration
+ACCOUNT=ezyang
+REPO=sozu
+
+git config --global user.name "Pushbot"
+git config --global user.email "pushbot@$(hostname)"
 git config --global push.default simple
 cp id_rsa $HOME/.ssh/id_rsa
 chmod 0600 $HOME/.ssh/id_rsa
@@ -9,5 +14,5 @@ git init
 git remote add origin git@github.com:ezyang/sozu-binaries.git
 cp ../main main
 git add .
-git commit -m '{"account":"ezyang", "repo":"sozu", "commit": "'$TRAVIS_COMMIT'"}'
+git commit -m '{"account":"'$ACCOUNT'", "repo":"'$REPO'", "commit": "'$TRAVIS_COMMIT'"}'
 git push -f origin "HEAD:rev-$TRAVIS_COMMIT"
